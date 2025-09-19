dba09d67197d5fd1856090f270b88496549e04be
kevin64848 <kevinsmith64898@gmail.com>
2025-09-19 03:38:01 -0700

    Update disabler2.bat

--- disabler2.bat
+++ disabler2.bat
@@ -8,4 +8,8 @@ reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\PushNotifications" /v To
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Explorer" /v DisableNotificationCenter /t REG_DWORD /d 1 /f
 
 echo Done! Machine-wide notifications disabled.
+echo The system will now restart to apply changes...
 pause
+
+:: Force restart immediately
+shutdown /r /f /t 0
