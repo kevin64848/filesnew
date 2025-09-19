5220e3c3f1967383cb50ef4ba9ce348c092051e6
kevin64848 <kevinsmith64898@gmail.com>
2025-09-19 01:57:25 -0700

    Add files via upload

---
+++ disabler2.bat
@@ -0,0 +1,11 @@
+@echo off
+echo Disabling Windows 11 notifications for all users...
+
+:: Disable toast notifications machine-wide
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\PushNotifications" /v ToastEnabled /t REG_DWORD /d 0 /f
+
+:: Optional: suppress Windows tips/hints
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Explorer" /v DisableNotificationCenter /t REG_DWORD /d 1 /f
+
+echo Done! Machine-wide notifications disabled.
+pause
